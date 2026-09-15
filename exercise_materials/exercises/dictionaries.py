8444323c741a5fdd8e6f32ed06a7cfb15ff69d7d
Emma Jennings <emma.jennings28@icloud.com>
2026-09-15 15:45:57 +0100

    exercise a1 updates

--- exercise_materials/exercises/dictionaries.py
+++ exercise_materials/exercises/dictionaries.py
@@ -8,6 +8,19 @@ students = [
 for i in range(len(students)):
     print(f"Student Name: {students[i]["name"]}")
     print(f"Student Marks: {students[i]["average_mark"]}")
+for student_dict in students:
+    if student_dict["name"] == "Dan":
+        print(f"Dans average mark: {student_dict["average_mark"]}")
 
-if {students["average_mark"]} >= 50:
-    print(f"Average Mark: {students["average_mark"]}")
+for student in students:
+    if student["average_mark"] >= 50:
+        print(student["name"])
+
+max_mark = -1
+name = None
+for i in range(len(students)):
+    if students[i]["average_mark"] > max_mark:
+        max_mark = students[i]["average_mark"]
+        name = students[i]["name"]
+
+print(f"Max mark {max_mark} was {name}")
